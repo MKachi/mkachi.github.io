ca62cd3f689c6a23563fdbd144a645a01fe7a623
MKachi <mkachi@naver.com>
2020-03-25 23:55:39 +0900

    Button 추가

---
+++ src/components/atoms/Button/index.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import classNames from 'classnames'
+import styles from './style.module.css'
+
+interface IProps {
+  className?: string
+  children?: React.ReactNode
+  text?: string
+  onClick?(event: React.FormEvent<HTMLButtonElement>): void
+}
+
+const Button: React.FC<IProps> = ({ className, children, text, onClick = () => {} }) => {
+  const classProps = classNames(className, styles.default)
+  return (
+    <button className={classProps} onClick={onClick}>
+      {text}
+      {children}
+    </button>
+  )
+}
+
+export default Button
