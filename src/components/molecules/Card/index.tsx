aba225cb1d6af43da90e74139146a9f0f08f0bce
MKachi <mkachi@naver.com>
2020-04-01 19:01:47 +0900

    Card Component 추가

---
+++ src/components/molecules/Card/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import classNames from 'classnames'
+import styles from './style.module.css'
+
+interface IProps {
+  className?: string
+  children?: React.ReactNode
+}
+
+const Card: React.FC<IProps> = ({ className, children }) => {
+  const classProps = classNames(className, styles.default)
+  return <section className={classProps}>{children}</section>
+}
+
+export default Card
