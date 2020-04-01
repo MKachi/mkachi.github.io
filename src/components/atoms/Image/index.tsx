e1af9f5e33e057f6c872c5114e50b2bd8b0e3656
MKachi <mkachi@naver.com>
2020-04-01 19:31:44 +0900

    Image Component 추가

---
+++ src/components/atoms/Image/index.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import classNames from 'classnames'
+import styles from './style.module.css'
+
+export enum Type {
+  Default = 'img',
+  Background = 'background'
+}
+
+interface IProps {
+  className?: string
+  type?: Type
+  src: string
+}
+
+const Image: React.FC<IProps> = ({ className, type = Type.Default, src }) => {
+  const classProps = classNames(className, styles[type])
+  if (type === Type.Default) {
+    return <img className={classProps} src={src} />
+  } else {
+    return <div className={classProps} style={{ backgroundImage: `url(${src})` }} />
+  }
+}
+
+export default Image
