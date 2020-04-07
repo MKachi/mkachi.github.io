39291b516159516e3fac833b151dadb96b8c13c8
MKachi <mkachi@naver.com>
2020-04-07 19:47:48 +0900

    A Component 추가

---
+++ src/components/atoms/A/index.tsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import classNames from 'classnames'
+import styles from './style.module.css'
+
+import { Link } from 'react-router-dom'
+
+export enum Type {
+  Route,
+  Url
+}
+
+interface IProps {
+  className?: string
+  to: string
+  type?: Type
+  children?: React.ReactNode
+}
+
+const A: React.FC<IProps> = ({ className, to, children, type = Type.Url }) => {
+  const classProps = classNames(className, styles.default)
+  if (type == Type.Route) {
+    return (
+      <Link className={classProps} to={to}>
+        {children}
+      </Link>
+    )
+  } else {
+    return (
+      <a className={classProps} href={to}>
+        {children}
+      </a>
+    )
+  }
+}
+
+export default A
