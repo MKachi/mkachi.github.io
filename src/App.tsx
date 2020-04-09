cb3e7e254fc0b21a43bdb4abf53b2ec7d828a943
MKachi <mkachi@naver.com>
2020-04-09 16:49:58 +0900

    App render 수정

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { HashRouter as Router, Switch } from 'react-router-dom'
 import router from './router'
+import './index.css'
 
-ReactDOM.render(
-  <Router>
-    <Switch>{router()}</Switch>
-  </Router>,
-  document.getElementById('root')
-)
+const App: React.FC = () => {
+  return (
+    <Router>
+      <Switch>{router()}</Switch>
+    </Router>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
