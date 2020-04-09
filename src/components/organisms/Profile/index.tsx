ce9ad3073462873befd1072710e917356c24feef
MKachi <mkachi@naver.com>
2020-04-09 09:48:04 +0900

    이메일 아이콘 제거

--- src/components/organisms/Profile/index.tsx
+++ src/components/organisms/Profile/index.tsx
@@ -20,10 +20,7 @@ const Profile: React.FC<IProps> = ({ className }) => {
         <Image className={styles.profile} type={Type.Background} src={'https://avatars1.githubusercontent.com/u/11822155?s=460&v=4'} />
         <div className={styles.description}>
           <P size={Size.Medium} text={'Username'} />
-          <Layout direction={Direction.Row} verticalAlign={VerticalAlign.Center}>
-            <MdMail className={styles['icon-mail']} />
-            <P size={Size.Small} text={'mkachi@naver.com'} />
-          </Layout>
+          <P size={Size.Small} text={'mkachi@naver.com'} />
           <div className={styles.icons}>
             <FaGithub className={styles.icon} />
             <FaDiscord className={styles.icon} />
