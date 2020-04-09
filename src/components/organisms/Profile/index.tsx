14e5d79169105d2e2c2db02b2e8076836d6b3771
MKachi <mkachi@naver.com>
2020-04-09 14:56:39 +0900

    Profile Component Property 추가 & 스타일 변경

--- src/components/organisms/Profile/index.tsx
+++ src/components/organisms/Profile/index.tsx
@@ -2,29 +2,69 @@ import React from 'react'
 import classNames from 'classnames'
 import styles from './style.module.css'
 
-import { MdMail } from 'react-icons/md'
 import { FaGithub, FaFacebook, FaRss } from 'react-icons/fa'
 
-import Layout, { Direction, VerticalAlign } from '../../molecules/Layout'
+import Layout, { Direction } from '../../molecules/Layout'
 import Image, { Type } from '../../atoms/Image'
 import P, { Size } from '../../atoms/P'
+import { IconType } from 'react-icons/lib/cjs'
+import A, { Type as LinkType } from '../../atoms/A/index'
 
 interface IProps {
   className?: string
+  profile: string
+  username: string
+  description?: string
+  github?: string
+  facebook?: string
+  rss?: string
 }
 
-const Profile: React.FC<IProps> = ({ className }) => {
+const createSocialLink = (icon: JSX.Element, link: string) => {
+  if (link == null || link == '') {
+    return null
+  }
+  return (
+    <A to={link} type={LinkType.Url}>
+      {icon}
+    </A>
+  )
+}
+
+const createSocialLinks = <T extends IconType>(github, facebook, icon) => {
+  const githubIcon = classNames(styles.icon, styles.github)
+  const facebookIcon = classNames(styles.icon, styles.facebook)
+  const rssIcon = classNames(styles.icon, styles.rss)
+
+  const result: JSX.Element[] = []
+  if (github != '' && github != null) {
+    result.push(<FaGithub className={githubIcon} />)
+  }
+
+  if (facebook != '' && facebook != null) {
+    result.push(<FaGithub className={facebookIcon} />)
+  }
+
+  if (icon != '' && icon != null) {
+    result.push(<FaGithub className={icon} />)
+  }
+}
+
+const Profile: React.FC<IProps> = ({ className, profile, username, description, github = '', facebook = '', rss = '' }) => {
+  const githubIcon = classNames(styles.icon, styles.github)
+  const facebookIcon = classNames(styles.icon, styles.facebook)
+  const rssIcon = classNames(styles.icon, styles.rss)
   return (
     <div>
       <Layout direction={Direction.Row}>
-        <Image className={styles.profile} type={Type.Background} src={'https://avatars1.githubusercontent.com/u/11822155?s=460&v=4'} />
+        <Image className={styles.profile} type={Type.Background} src={profile} />
         <div className={styles.description}>
-          <P size={Size.Medium} text={'Username'} />
-          <P size={Size.Small} text={'mkachi@naver.com'} />
+          <P size={Size.Medium} text={username} />
+          <P size={Size.Small} text={description} />
           <div className={styles.icons}>
-            <FaGithub className={styles.icon} />
-            <FaFacebook className={styles.icon} />
-            <FaRss className={styles.icon} />
+            {createSocialLink(<FaGithub className={githubIcon} />, github)}
+            {createSocialLink(<FaFacebook className={facebookIcon} />, facebook)}
+            {createSocialLink(<FaRss className={rssIcon} />, rss)}
           </div>
         </div>
       </Layout>
