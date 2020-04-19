c31ec5e99fe184fb204526bb0f716c552765b012
MKachi <mkachi@naver.com>
2020-04-19 20:02:00 +0900

    Page -> Frame 으로 이름 변경, FrameType 추가

---
+++ src/components/molecules/Frame/index.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import classNames from 'classnames'
+import styles from './style.module.css'
+
+export enum FrameType {
+  Container = 'container',
+  Content = 'content',
+}
+
+interface IProps {
+  className?: string
+  type?: FrameType
+  children?: React.ReactNode
+}
+
+const Frame: React.FC<IProps> = ({ className, type = FrameType.Container, children }) => {
+  const classProps = classNames(className, styles.default, styles[type])
+  return <div className={classProps}>{children}</div>
+}
+
+export default Frame
