b2c85c5e46a3d0e797a52e7a834aa9af1eb4800d
MKachi <mkachi@naver.com>
2020-05-24 00:33:01 +0900

    Button property 수정

--- src/components/atoms/Button/index.tsx
+++ src/components/atoms/Button/index.tsx
@@ -5,15 +5,13 @@ import styles from './style.module.css'
 interface IProps {
   className?: string
   children?: React.ReactNode
-  text?: string
   onClick?(event: React.FormEvent<HTMLButtonElement>): void
 }
 
-const Button: React.FC<IProps> = ({ className, children, text, onClick = () => {} }) => {
+const Button: React.FC<IProps> = ({ className, children, onClick = () => {} }) => {
   const classProps = classNames(className, styles.default)
   return (
     <button className={classProps} onClick={onClick}>
-      {text}
       {children}
     </button>
   )
