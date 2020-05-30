049993ba7570cbf6a70ba5c9350ae7eaee468f36
MKachi <mkachi@naver.com>
2020-05-30 21:04:23 +0900

    Tag Component onClick 이벤트 추가

--- src/components/atoms/Tag/index.tsx
+++ src/components/atoms/Tag/index.tsx
@@ -5,11 +5,16 @@ import styles from './style.module.css'
 interface IProps {
   className?: string
   children?: React.ReactNode
+  onClick(): void
 }
 
-const Tag: React.FC<IProps> = ({ className, children }) => {
+const Tag: React.FC<IProps> = ({ className, children, onClick = () => {} }) => {
   const classProps = classNames(className, styles['default'])
-  return <span className={classProps}>{children}</span>
+  return (
+    <span className={classProps} onClick={onClick}>
+      {children}
+    </span>
+  )
 }
 
 export default Tag
