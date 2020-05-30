3f0deeeab25b779228f234336bea5eaf7286bd72
MKachi <mkachi@naver.com>
2020-05-30 20:57:18 +0900

    Tag 목록이 벗어나도 잘 보이도록 수정

--- src/components/organisms/PostList/index.tsx
+++ src/components/organisms/PostList/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import styles from './style.module.css'
 
-import Layout, { Direction } from '../../molecules/Layout'
+import Layout, { Direction, Wrap } from '../../molecules/Layout'
 import Card from '../../molecules/Card'
 import A, { LinkType } from '../../atoms/A'
 import P from '../../atoms/P'
@@ -30,7 +30,7 @@ const PostList: React.FC<IProps> = ({ className, pageIndex, showPostCount, posts
               <A className={styles['post-link']} to={`/post/${post.key}`} type={LinkType.Route}>
                 <P className={styles['title']} text={post.title} />
               </A>
-              <Layout className={styles['tag-list']} direction={Direction.Row}>
+              <Layout className={styles['tag-list']} direction={Direction.Row} wrap={Wrap.Wrap}>
                 {post.tags.map(tag => <Tag>{`#${tag}`}</Tag>)}
               </Layout>
             </Layout>
--- src/components/organisms/TagList/index.tsx
+++ src/components/organisms/TagList/index.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import classNames from 'classnames'
 import styles from './style.module.css'
 
-import Layout, { Direction, Wrap, Overflow } from '../../molecules/Layout'
+import Layout, { Direction, Wrap } from '../../molecules/Layout'
 import Tag from '../../atoms/Tag'
 import { ITag } from '../../../models/tag'
 
@@ -14,7 +14,7 @@ interface IProps {
 const TagList: React.FC<IProps> = ({ className, tags }) => {
   const classProps = classNames(className, styles['default'])
   return (
-    <Layout className={classProps} direction={Direction.Row} overflow={Overflow.Auto}>
+    <Layout className={classProps} direction={Direction.Row} wrap={Wrap.Wrap}>
       {tags.map(value => {
         return <Tag className={styles['tag']}>{`#${value.name} | ${value.posts.length}`}</Tag>
       })}
