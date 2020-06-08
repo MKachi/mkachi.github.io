ee2945245b913885ba418bad39190d9cf8dae79a
MKachi <mkachi@naver.com>
2020-06-08 11:07:00 +0900

    SearchBox Component 추가

--- src/components/organisms/SearchBox/index.tsx
+++ src/components/organisms/SearchBox/index.tsx
@@ -2,28 +2,43 @@ import React, { useState } from 'react'
 import classNames from 'classnames'
 import styles from './style.module.css'
 
-import Layout from '../../molecules/Layout'
+import Layout, { Direction } from '../../molecules/Layout'
 import TextBox from '../../atoms/TextBox/index'
+import Button from '../../atoms/Button'
+
+import useMenu from '../../../hooks/useMenu'
+
+import { IoMdClose } from 'react-icons/io'
 
 interface IProps {
   className?: string
+  searchText?: string
   placeholder?: string
-  onChange?(inputText: string): void
+  onChange?(event: React.ChangeEvent<HTMLInputElement>): void
 }
 
-const SearchBox: React.FC<IProps> = ({ className, placeholder = '', onChange = (inputText: string) => {} }) => {
+const SearchBox: React.FC<IProps> = ({ className, searchText = '', placeholder = '', onChange = () => {} }) => {
+  const { setSearchMode } = useMenu()
+  const [boxFocused, setBoxFocus] = useState(false)
+
   const classProps = classNames(className, styles['default'])
+  const buttonProps = boxFocused ? styles['button-focus'] : styles['button']
 
   return (
-    <Layout className={classProps}>
+    <Layout className={classProps} direction={Direction.Row}>
       <TextBox
-        value={inputText}
-        onChange={() => {
-          // onChange(inputText)
-          console.log(inputText)
-        }}
+        className={styles['search-box']}
+        value={searchText}
+        onChange={onChange}
+        onFocus={() => setBoxFocus(true)}
+        onBlur={() => setBoxFocus(false)}
         placeholder={placeholder}
       />
+      <Button className={buttonProps} onClick={() => setSearchMode(false)}>
+        <span className={styles['close-icon']}>
+          <IoMdClose />
+        </span>
+      </Button>
     </Layout>
   )
 }
