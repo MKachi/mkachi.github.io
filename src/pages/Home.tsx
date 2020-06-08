d95f11ca904c7df5a1f7afa6e7aeae48cec17926
MKachi <mkachi@naver.com>
2020-06-08 11:08:23 +0900

    SearchMenu Render 로직 추가

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import useDB from '../hooks/useDB'
 import useMenu from '../hooks/useMenu'
 
@@ -10,20 +10,63 @@ import TabMenu from '../components/organisms/TabMenu'
 import TagList from '../components/organisms/TagList'
 import PostListTemplate from '../components/templates/PostListTemplate'
 import SearchBox from '../components/organisms/SearchBox'
-import TextBox from '../components/atoms/TextBox'
 
-const menuItems = [ 'Archive', 'Tags' ]
+import { IPost } from '../models/post'
+import { ITag } from '../models/tag'
+
+const menuItems = ['Archive', 'Tags']
 
 const isSelectedMenu = (menuName: string): boolean => {
-  const { menuIndex, searchMode } = useMenu()
-  if (searchMode) {
-    return false
-  }
+  const { menuIndex } = useMenu()
   return menuItems[menuIndex] === menuName
+}
+const showPostList = (posts: IPost[], tags: ITag[]) => {
+  return (
+    <React.Fragment>
+      <TabMenu items={menuItems} />
+      {isSelectedMenu('Archive') && <PostListTemplate posts={posts} />}
+      {isSelectedMenu('Tags') && <TagList tags={tags} />}
+    </React.Fragment>
+  )
+}
+
+const getSearchPosts = (searchText: string, posts: IPost[]) => {
+  const result: IPost[] = []
+
+  if (searchText.indexOf('tags:') === 0) {
+    const tagList = searchText.replace(/#|tags:| /gi, '').split(',')
+    tagList.forEach(target => {
+      posts.forEach(post => {
+        if (post.tags.includes(target)) {
+          result.push(post)
+        }
+      })
+    })
+  } else {
+    posts.forEach(post => {
+      if (post.title.includes(searchText)) {
+        result.push(post)
+      }
+    })
+  }
+  return result
+}
+const showSearchList = (posts: IPost[], searchText: string, setSearchText: (value: string) => void) => {
+  return (
+    <React.Fragment>
+      <SearchBox searchText={searchText} onChange={(event: React.ChangeEvent<HTMLInputElement>) => setSearchText(event.target.value)} />
+      <PostListTemplate posts={getSearchPosts(searchText, posts)} />
+    </React.Fragment>
+  )
 }
 
 const Home = () => {
+  const [searchText, setSearchText] = useState('')
   const { posts, tags } = useDB()
+  const { searchMode } = useMenu()
+
+  if (searchMode) {
+  }
 
   console.log('render Home page')
   return (
@@ -31,9 +74,7 @@ const Home = () => {
       <Frame type={FrameType.Container}>
         <Layout direction={Direction.Row} horizontalAlign={HorizontalAlign.SpaceAround}>
           <Profile
-            profile={
-              'https://avatars3.githubusercontent.com/u/11822155?s=460&u=1baad602f7e934445410c001377b900a67271b22&v=4'
-            }
+            profile={'https://avatars3.githubusercontent.com/u/11822155?s=460&u=1baad602f7e934445410c001377b900a67271b22&v=4'}
             username={'M_Kachi'}
             description={'adsfadsfas'}
             github={'https://github.com/mkachi'}
@@ -41,11 +82,7 @@ const Home = () => {
             rss={'https://www.facebook.com/mkachi'}
           />
         </Layout>
-        <TextBox />
-        {/* <SearchBox /> */}
-        <TabMenu items={menuItems} />
-        {isSelectedMenu('Archive') && <PostListTemplate posts={posts} />}
-        {isSelectedMenu('Tags') && <TagList tags={tags} />}
+        {searchMode ? showSearchList(posts, searchText, (value: string) => setSearchText(value)) : showPostList(posts, tags)}
       </Frame>
       <Footer />
     </Frame>
