3cc36e9a3c8ec9fa072b6006aea3292306b72cb4
MKachi <mkachi@naver.com>
2020-06-13 22:00:14 +0900

    Post 게시물 스타일 작성

---
+++ src/components/atoms/Comment/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import classNames from 'classnames'
+import styles from './style.module.css'
+
+interface IProps {
+  className?: string
+}
+
+const Comment: React.FC<IProps> = ({ className }) => {
+  return <div />
+}
+
+export default Comment
--- src/components/templates/PostTemplate/index.tsx
+++ src/components/templates/PostTemplate/index.tsx
@@ -4,7 +4,7 @@ import styles from './style.module.css'
 
 import Tag from '../../atoms/Tag'
 import { IPost, IPostContent } from '../../../models/post'
-import Layout, { Direction } from '../../molecules/Layout'
+import Layout, { Direction, Wrap } from '../../molecules/Layout'
 
 interface IProps {
   className?: string
@@ -22,8 +22,8 @@ const PostTemplate: React.FC<IProps> = ({ className, info, content }) => {
           <span>{info.date}</span>
           <span>{info.time}</span>
         </Layout>
-        <Layout className={styles['post-tags']} direction={Direction.Row}>
-          {info.tags.map(tag => <Tag value={tag}>{`#${tag}`}</Tag>)}
+        <Layout className={styles['post-tags']} direction={Direction.Row} wrap={Wrap.Wrap}>
+          {info.tags.map(tag => <Tag className={styles['tag']} value={tag}>{`#${tag}`}</Tag>)}
         </Layout>
       </Layout>
       <div className={styles['post-content']} dangerouslySetInnerHTML={{ __html: content.content }} />
