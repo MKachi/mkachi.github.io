05fd20fe312182b7a79ca50f0fc13c43f9be2dc2
MKachi <mkachi@naver.com>
2020-06-13 18:07:36 +0900

    Tag margin 수정

--- src/components/organisms/PostList/index.tsx
+++ src/components/organisms/PostList/index.tsx
@@ -31,7 +31,7 @@ const PostList: React.FC<IProps> = ({ className, pageIndex, showPostCount, posts
                 <P className={styles['title']} text={post.title} />
               </A>
               <Layout className={styles['tag-list']} direction={Direction.Row} wrap={Wrap.Wrap}>
-                {post.tags.map((tag, index) => <Tag key={tag} value={tag}>{`#${tag}`}</Tag>)}
+                {post.tags.map((tag, index) => <Tag className={styles['tag']} key={tag} value={tag}>{`#${tag}`}</Tag>)}
               </Layout>
             </Layout>
           </Card>
