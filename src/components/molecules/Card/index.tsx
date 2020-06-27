fd0e6237f18b51f7db05f7d37c90f4ffa2f7c9a2
MKachi <mkachi@naver.com>
2020-06-27 19:24:44 +0900

    Card Component Resume Type 추가

--- src/components/molecules/Card/index.tsx
+++ src/components/molecules/Card/index.tsx
@@ -2,13 +2,19 @@ import React from 'react'
 import classNames from 'classnames'
 import styles from './style.module.css'
 
+export enum CardType {
+  Post = 'post',
+  Resume = 'resume',
+}
+
 interface IProps {
   className?: string
   children?: React.ReactNode
+  type?: CardType
 }
 
-const Card: React.FC<IProps> = ({ className, children }) => {
-  const classProps = classNames(className, styles['default'])
+const Card: React.FC<IProps> = ({ className, children, type = CardType.Post }) => {
+  const classProps = classNames(className, styles[type])
   return <section className={classProps}>{children}</section>
 }
 
