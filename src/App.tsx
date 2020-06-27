4cd0d3c3a82a6866538da2816e445261dc07a6d6
MKachi <mkachi@naver.com>
2020-06-28 02:10:16 +0900

    재미있는거 추가

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import './index.css'
 import MenuProvider from './contexts/MenuContext'
 
 const App: React.FC = () => {
+  console.log('개발자 도구에 당도한것을 환영하오 낯선이여')
   return (
     <Router>
       <MenuProvider>
