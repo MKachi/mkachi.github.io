ddd710f3ba303e3d74e00e7adb3781d5fc07e9c8
MKachi <mkachi@naver.com>
2020-07-05 02:12:06 +0900

    프로젝트 하단에 아이콘 추가

--- src/components/templates/ResumeTemplate/index.tsx
+++ src/components/templates/ResumeTemplate/index.tsx
@@ -5,15 +5,33 @@ import styles from './style.module.css'
 import dayjs from 'dayjs'
 import { MdMailOutline } from 'react-icons/md'
 
-import Layout, { Direction, VerticalAlign } from '../../molecules/Layout'
+import Layout, { Direction, VerticalAlign, Wrap } from '../../molecules/Layout'
 import Image from '../../atoms/Image'
 import P from '../../atoms/P'
 import { FaBloggerB, FaGithub } from 'react-icons/fa'
-import A from '../../atoms/A'
+import A, { LinkType } from '../../atoms/A'
 import Card, { CardType } from '../../molecules/Card'
 import ListGroup, { ListType } from '../../atoms/ListGroup'
 import Timeline from '../../organisms/Timeline'
 import TimeComment from '../../organisms/TimeComment'
+import IconLabel from '../../organisms/IconLabel'
+
+import icon_csharp from '../../../assets/tech-icons/csharp.png'
+import icon_astm from '../../../assets/tech-icons/astm.png'
+import icon_hl7 from '../../../assets/tech-icons/hl7.png'
+import icon_vbnet from '../../../assets/tech-icons/vbnet.png'
+import icon_devextreme from '../../../assets/tech-icons/devextreme.png'
+import icon_postcss from '../../../assets/tech-icons/postcss.png'
+import icon_react from '../../../assets/tech-icons/react.png'
+import icon_redux from '../../../assets/tech-icons/redux.png'
+import icon_tailwind from '../../../assets/tech-icons/tailwindcss.png'
+import icon_typescript from '../../../assets/tech-icons/typescript.png'
+import icon_udp from '../../../assets/tech-icons/udp.png'
+import icon_wpf from '../../../assets/tech-icons/wpf.png'
+
+import icon_unity from '../../../assets/tech-icons/unity.png'
+import icon_cocos2dx from '../../../assets/tech-icons/cocos2dx.png'
+import icon_cpp from '../../../assets/tech-icons/cpp.png'
 
 interface IProps {
   className?: string
@@ -72,7 +90,7 @@ const Resume: React.FC<IProps> = ({ className }) => {
               </tr>
               <tr>
                 <td className={styles['resume-table-padding']}>{'17.03 ~ 휴학중'}</td>
-                <td className={styles['resume-table-padding']}>{'성공회대학교 컴퓨터공학과 (산업기능요원근무로 휴학)'}</td>
+                <td className={styles['resume-table-padding']}>{'성공회대학교 컴퓨터공학과'}</td>
               </tr>
               <tr>
                 <td>{'14.03 ~ 17.02'}</td>
@@ -126,36 +144,65 @@ const Resume: React.FC<IProps> = ({ className }) => {
           <TimeComment>
             <P>{'2020.06 ~ 진행중'}</P>
             <P>{'한양대학교서울병원, 한양대학교구리병원 인터페이스 개발 지원'}</P>
+            <Layout direction={Direction.Row} wrap={Wrap.Wrap}>
+              <IconLabel icon={icon_csharp} text={'C#'} />
+              <IconLabel icon={icon_vbnet} text={'VB.NET'} />
+              <IconLabel icon={icon_astm} text={'ASTM'} />
+              <IconLabel icon={icon_hl7} text={'HL7'} />
+            </Layout>
           </TimeComment>
 
           <TimeComment>
             <P>{'2020.04 ~ 2020.04'}</P>
             <P>{'COVID-19 검사 자동 판정 솔루션 개발 지원'}</P>
+            <Layout direction={Direction.Row} wrap={Wrap.Wrap}>
+              <IconLabel icon={icon_react} text={'React'} />
+              <IconLabel icon={icon_redux} text={'Redux'} />
+              <IconLabel icon={icon_typescript} text={'Typescript'} />
+              <IconLabel icon={icon_devextreme} text={'DevExtreme'} />
+              <IconLabel icon={icon_postcss} text={'PostCSS'} />
+              <IconLabel icon={icon_tailwind} text={'Tailwind'} />
+            </Layout>
           </TimeComment>
 
           <TimeComment>
             <P>{'2020.03 ~ 2020.03'}</P>
             <P>{'수원삼성전기부속의원 바코드 프린트 인터페이스 개발'}</P>
+            <Layout direction={Direction.Row} wrap={Wrap.Wrap}>
+              <IconLabel icon={icon_csharp} text={'C#'} />
+              <IconLabel icon={icon_wpf} text={'WPF'} />
+              <IconLabel icon={icon_devextreme} text={'DevExpress'} />
+            </Layout>
           </TimeComment>
 
           <TimeComment>
             <P>{'2019.12 ~ 2020.01'}</P>
             <P>{'서초삼성부속의원 바코드 프린트 인터페이스 개발'}</P>
-          </TimeComment>
-
-          <TimeComment>
-            <P>{'2019.12 ~ 2020.01'}</P>
-            <P>{'서초삼성부속의원 바코드 프린트 인터페이스 개발'}</P>
+            <Layout direction={Direction.Row} wrap={Wrap.Wrap}>
+              <IconLabel icon={icon_csharp} text={'C#'} />
+              <IconLabel icon={icon_wpf} text={'WPF'} />
+              <IconLabel icon={icon_devextreme} text={'DevExpress'} />
+            </Layout>
           </TimeComment>
 
           <TimeComment>
             <P>{'2019.10 ~ 2019.12'}</P>
             <P>{'NihonkoDen Patient Monitor 정보 수집 솔루션 개발'}</P>
+            <Layout direction={Direction.Row} wrap={Wrap.Wrap}>
+              <IconLabel icon={icon_csharp} text={'C#'} />
+              <IconLabel icon={icon_udp} text={'UDP'} />
+            </Layout>
           </TimeComment>
 
           <TimeComment>
             <P>{'2018.11 ~ 진행중'}</P>
             <P>{'고신대학교복음병원 인터페이스 유지 보수 및 신규 개발'}</P>
+            <Layout direction={Direction.Row} wrap={Wrap.Wrap}>
+              <IconLabel icon={icon_csharp} text={'C#'} />
+              <IconLabel icon={icon_vbnet} text={'VB.NET'} />
+              <IconLabel icon={icon_astm} text={'ASTM'} />
+              <IconLabel icon={icon_hl7} text={'HL7'} />
+            </Layout>
           </TimeComment>
 
           <TimeComment>
@@ -171,6 +218,14 @@ const Resume: React.FC<IProps> = ({ className }) => {
           <TimeComment>
             <P>{'2017.08 ~ 2017.12'}</P>
             <P>{'소프트웨어 마에스트로 8기 수료'}</P>
+          </TimeComment>
+
+          <TimeComment>
+            <P>{'2014.10 ~ 2015.02'}</P>
+            <P>{'화학식이 어려워'}</P>
+            <A to={'https://youtu.be/O2yWHDXbCdQ'} type={LinkType.Url}>
+              {'https://youtu.be/O2yWHDXbCdQ'}
+            </A>
           </TimeComment>
         </Timeline>
       </Layout>
